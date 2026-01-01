6ff45ac38310107ab951891946cdad98a339887c
unknown <mr.shahbaznns@gmail.com>
2026-01-01 16:06:08 +0500

    HasUuid update User

--- app/Models/User.php
+++ app/Models/User.php
@@ -10,11 +10,11 @@ use Laravel\Fortify\TwoFactorAuthenticatable;
 use Laravel\Jetstream\HasProfilePhoto;
 use Laravel\Jetstream\HasTeams;
 use Laravel\Sanctum\HasApiTokens;
-
+use App\Traits\HasUuid;
 class User extends Authenticatable
 {
     use HasApiTokens;
-
+    use HasUuid;
     /** @use HasFactory<\Database\Factories\UserFactory> */
     use HasFactory;
     use HasProfilePhoto;
