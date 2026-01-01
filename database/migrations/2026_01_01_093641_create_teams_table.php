3cb3b24cc188c51efdd41c0e8cf7c29cb646f5b8
unknown <mr.shahbaznns@gmail.com>
2026-01-01 15:12:24 +0500

    first commit

--- database/migrations/2026_01_01_093641_create_teams_table.php
+++ database/migrations/2026_01_01_093641_create_teams_table.php
@@ -3,7 +3,8 @@
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
-
+use Illuminate\Support\Str;
+use App\Traits\HasUuid;
 return new class extends Migration
 {
     /**
@@ -12,7 +13,7 @@ return new class extends Migration
     public function up(): void
     {
         Schema::create('teams', function (Blueprint $table) {
-            $table->id();
+            $table->uuid('id')->primary();
             $table->foreignId('user_id')->index();
             $table->string('name');
             $table->boolean('personal_team');
